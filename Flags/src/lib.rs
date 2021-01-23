3ce257de7653d556bfebf1b54f3479255220a4ea
kwsantiago <kwsantiago@usf.edu>
2021-01-23 10:55:05 -0500

    Added Flags contract functions

--- Flags/src/lib.rs
+++ Flags/src/lib.rs
@@ -27,4 +27,55 @@ impl Flags {
         }
         return responses;
     }
+
+    pub fn raiseFlag(&mut self, subject: AccountId) {
+        assert!(self.allowedToRaiseFlags(), "Not allowed to raise flags");
+
+        self.tryToRaiseFlag(subject);
+    }
+
+    pub fn raiseFlags(&mut self, subjects: AccountId[]) {
+        assert!(self.allowedToRaiseFlags(), "Not allowed to raise flags");
+
+        for i in 0..subjects.len() {
+            self.tryToRaiseFlag(subjects[i]);
+        }
+    }
+
+    pub fn lowerFlags(&mut self, subjects: AccountId[]) {
+        self.onlyOwner();
+        for i in 0..subjects.len() {
+            let subject: AccountId = subjects[i];
+
+            if(self.flags[subject]) {
+                self.flags[subject] = false;
+            }
+        }
+    }
+
+    pub fn setRaisingAccessController(&mut self, racAddress: AccountId) {
+        self.onlyOwner();
+        let previous: AccountId = self.raisingAccessController;
+
+        if(previous != racAddress) {
+            self.raisingAccessController = racAddress;
+        }
+    }
+
+    // PRIVATE
+
+    fn allowedToRaiseFlags(&mut self) -> bool {
+        // Check owner syntax and msg.data
+        return env::predecessor_account_id() == owner || self.raisingAccessController.hasAccess(env::predecessor_account_id(), msg.data);
+    }
+
+    fn tryToRaiseFlag(&mut self, subject: AccountId) {
+        if(!self.flags[subject]) {
+            self.flags[subject] = true;
+        }
+    }
+
+    fn onlyOwner(&mut self) {
+        assert_eq!(env::signer_account_id(), env::current_account_id(), "Only contract owner can call this method.");
+    }
 }
