ed7bec2fa3bfafcdb378757e61d90c44780756b3
John Doe <nolanjacobson@protonmail.com>
2021-02-10 23:00:32 -0600

    add boilerplate for unit testing

--- AccessControlledAggregator/src/lib.rs
+++ AccessControlledAggregator/src/lib.rs
@@ -618,3 +618,34 @@ impl AccessControlledAggregator {
         assert!(self.hasAccess(env::predecessor_account_id()), "No access")
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+    …
+    fn get_context(predecessor_account_id: String, storage_usage: u64) -> VMContext {
+        VMContext {
+            current_account_id: "alice.testnet".to_string(),
+            signer_account_id: "jane.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id,
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    // Tests
+}
