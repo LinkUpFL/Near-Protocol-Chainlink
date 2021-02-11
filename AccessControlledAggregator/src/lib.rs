375b20685a52f6f570317ba5f6ccf27de70bd472
kwsantiago <kwsantiago@usf.edu>
2021-02-10 19:07:10 -0500

    Updated function parameters and added validRoundId function

--- AccessControlledAggregator/src/lib.rs
+++ AccessControlledAggregator/src/lib.rs
@@ -1,23 +1,23 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Serialize, Deserialize};
-use near_sdk::collections::{TreeMap, UnorderedSet};
+use near_sdk::collections::{LookupMap};
 use near_sdk::json_types::{U128, U64};
 use near_sdk::{AccountId, env, near_bindgen, PromiseResult};
 use serde_json::json;
 use std::str;
-use std::collections::HashMap;
 
 pub type Base64String = String;
 
 #[derive(Serialize, Deserialize)]
 pub struct Round {
-    answer: i256,
+    answer: u128,
     startedAt: u64,
     updatedAt: u64,
     answeredInRound: u64
 }
 
 const V3_NO_DATA_ERROR: Base64String = "No data present";
+const ROUND_MAX: u64 = 2.pow(32-1);
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
@@ -28,7 +28,7 @@ pub struct AccessControlledAggregator {
 
 #[near_bindgen]
 impl AccessControlledAggregator {
-    pub fn getRoundData(&self, _roundId: U128) -> (roundId: u128, answer: i256, startedAt: u256, updatedAt: u256, answeredInRound: u80) {
+    pub fn getRoundData(&self, _roundId: U128) -> (roundId: u128, answer: u128, startedAt: u64, updatedAt: u64, answeredInRound: u64) {
         let roundId_u128: u128 = _roundId.into();
         let r: Round = self.rounds[roundId_u128 as u64];
 
@@ -43,24 +43,23 @@ impl AccessControlledAggregator {
         )
     }
 
-    pub fn latestRoundData(&self) -> (roundId: u80, answer: i256, startedAt: u256, updatedAt: u256, answeredInRound: u80) {
+    pub fn latestRoundData(&self) -> (roundId: u128, answer: u64, startedAt: u64, updatedAt: u64, answeredInRound: u128) {
         self.getRoundData(self.latestRoundId)
     }
 
-    pub fn latestAnswer(&self) -> i256 {
+    pub fn latestAnswer(&self) -> u128 {
         self.rounds[self.latestRoundId].answer
     }
 
-    pub fn latestRound(&self) -> u256 {
+    pub fn latestRound(&self) -> u64 {
         self.latestRoundId
     }
 
-    pub fn latestTimestamp(&self) -> u256 {
+    pub fn latestTimestamp(&self) -> u64 {
         self.rounds[self.latestRoundId].updatedAt
     }
 
-
-    pub fn getAnswer(&self, _roundId: U128) -> i256 {
+    pub fn getAnswer(&self, _roundId: U128) -> u128 {
         let roundId_u128: u128 = _roundId.into();
         if(self.validRoundId(_roundId)) {
             return self.rounds[roundId_u128 as u64].answer;
@@ -68,11 +67,18 @@ impl AccessControlledAggregator {
         return 0;
     }
 
-    pub fn getTimestamp(_roundId: U128) -> u256 {
+    pub fn getTimestamp(&self, _roundId: U128) -> u128 {
         let roundId_u128: u128 = _roundId.into();
         if(self.validRoundId(_roundId)) {
             return self.rounds[roundId_u128 as u64].answer;
         }
         return 0;
+    }
+
+    // Internal
+
+    fn validRoundId(&self, _roundId: U128) -> bool {
+        let roundId_u128: u128 = _roundId.into();
+        _roundId <= ROUND_MAX
     }
 }
