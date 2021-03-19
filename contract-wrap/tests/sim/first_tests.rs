f9f7da137922581753f3b0c888be54dd6a3f9cd2
John Doe <nolanjacobson@protonmail.com>
2021-03-19 17:32:22 -0600

    debugging update_available_funds fn

--- contract-wrap/tests/sim/first_tests.rs
+++ contract-wrap/tests/sim/first_tests.rs
@@ -1,21 +1,67 @@
-// use near_sdk_sim::{init_simulator, to_yocto, STORAGE_AMOUNT};
+use near_sdk::json_types::U128;
+use near_sdk::serde_json::json;
+use near_sdk_sim::DEFAULT_GAS;
 
-// const CONTRACT_ID: &str = "contract";
+use crate::utils::init_without_macros as init;
 
-// pub fn init() -> (UserAccount, UserAccount, UserAccount) {
-//     // Use `None` for default genesis configuration; more info below
-//     let root = init_simulator(None);
+#[test]
 
-//     let contract = root.deploy(
-//         &CONTRACT_WASM_BYTES,
-//         CONTRACT_ID.to_string(),
-//         STORAGE_AMOUNT // attached deposit
-//     );
+fn simulate_linktoken_transfer() {
+    let (root, aca, link, _oracle_one) = init();
+    // Transfer from link_token contract to ACA.
+    root.call(
+        link.account_id(),
+        "transfer_from",
+        &json!({
+            "owner_id": root.account_id().to_string(),
+            "new_owner_id": aca.account_id().to_string(),
+            "amount": "220"
+        })
+        .to_string()
+        .into_bytes(),
+        DEFAULT_GAS,
+        36500000000000000000000, // deposit
+    )
+    .assert_success();
+    root.call(
+        aca.account_id(),
+        "update_available_funds",
+        &json!({}).to_string().into_bytes(),
+        DEFAULT_GAS,
+        0, // deposit
+    )
+    .assert_success();
 
-//     let alice = root.create_user(
-//         "alice".to_string(),
-//         to_yocto("100") // initial balance
-//     );
-
-//     (root, contract, alice)
-// }
+    let _root_balance: U128 = root
+        .view(
+            link.account_id(),
+            "get_balance",
+            &json!({
+                "owner_id": root.valid_account_id()
+            })
+            .to_string()
+            .into_bytes(),
+        )
+        .unwrap_json();
+    let aca_balance: U128 = aca
+        .view(
+            link.account_id(),
+            "get_balance",
+            &json!({
+                "owner_id": aca.valid_account_id()
+            })
+            .to_string()
+            .into_bytes(),
+        )
+        .unwrap_json();
+    // let _available_funds: U128 = root
+    //     .view(
+    //         aca.account_id(),
+    //         "available_funds",
+    //         &json!({}).to_string().into_bytes(),
+    //     )
+    //     .unwrap_json();
+    // println!("{:?}", available_funds);
+    println!("{:?}", aca_balance);
+    assert_eq!(true, true);
+}
